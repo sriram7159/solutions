8d0d31a321d391f5238f8dbf9615fd486c62e062
sriram7159 <sriramvakani111@gmail.com>
2026-04-10 02:08:42 +0530

    Add: can-place-flowers - submission-2

---
+++ Data Structures & Algorithms/can-place-flowers/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canPlaceFlowers(self, arr: List[int], n: int) -> bool:
+        count = 0
+        if(len(arr)==1):
+            return True
+        for i in range(1,len(arr)-1):
+            if arr[i-1]==0 and arr[i+1]==0:
+                count+=1
+        
+        if count>=n:
+            return True
+        else:
+            return False
+
